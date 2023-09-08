011d52c4d8be954c67f2f8cc3663166f6543f17f
Andre <andrehristu@gmail.com>
2023-09-08 16:27:56 -0400

    need to determine dependencies

--- app/views.py
+++ app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # This is the api
+# Need to figure out how we getting CORS and stuff up in here
+
 def index(request):
     return HttpResponse("Waddup Otakus. You're at the AnimeOasis index.")
