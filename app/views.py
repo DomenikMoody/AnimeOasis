1a3270011e2a315c48e2a0f8e3edb53af7af12b4
Andre <andrehristu@gmail.com>
2023-09-08 15:35:18 -0400

    setup basic api

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Waddup Otakus. You're at the AnimeOasis index.")
