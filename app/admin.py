6949956b700bd0071a817c039f17a4fc1cf231e7
Andre <andrehristu@gmail.com>
2023-09-11 15:41:51 -0400

    models all connected to admin site

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 
+from .models import Anime, Playlist, User, Comment, Rating
 # Register your models here.
-
+# This lets us play with our data on the admin site
+admin.site.register([
+    Anime, Playlist, User, Comment, Rating
+])
