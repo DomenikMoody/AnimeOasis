3e4bec024550ae4ecf08555849098a309a44cdca
Andre <andrehristu@gmail.com>
2023-09-11 14:43:58 -0400

    more model set up and annotations

--- app/models.py
+++ app/models.py
@@ -10,23 +10,28 @@ from django.db import models
 #   that references it
 
 class Anime(models.Model):
-    pass
+    external_api_key = models.CharField(primary_key=True)
+
 
 class User(models.Model):
     username = models.CharField("username", max_length=100, unique=True)
     hashed_password = models.CharField("password", max_length=50)
     bio = models.CharField("bio", max_length=500)
     display_name = models.CharField(max_length=100, unique=False)
-    favorite_show = models.OneToOneField(
+    favorite_show = models.ForeignKey(
         Anime,
+        on_delete=models.SET_NULL,
     )
+    # models.ForeignKey defines a many-to-one relationship;
+    # A user has one favorite anime,
+    # but one anime can be favorited by many users
+
+class Comment(models.Model):
+    anime = models.ForeignKey
+
+class Review(models.Model):
+    pass
 
 
 class Playlist(models.Model):
-    pass
-
-class Comment(models.Model):
-    pass
-
-class Review(models.Model):
     pass
