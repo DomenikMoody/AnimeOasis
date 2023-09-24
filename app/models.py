694a248118b1868094a044ad1ae81ba6cc2b1880
Andre <andrehristu@gmail.com>
2023-09-24 13:30:43 -0400

    User model now inherits from django AbstractUser

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 # "python manage.py makemigrations app"
 #   creates the migration under app.migrations
@@ -22,6 +23,7 @@ class Anime(models.Model):
     # I think all we need is the api key?
     # unless we want to collect more data on each anime
     # as users query the external api for them
+
     def get_average_rating(self):
         """
         Queries for all of an anime's ratings
@@ -31,11 +33,36 @@ class Anime(models.Model):
         #  Anime.get_average_rating in the Views
         pass
 
-class User(models.Model):
-    username = models.CharField("username", max_length=100, unique=True)
-    hashed_password = models.CharField("password", max_length=50)
+
+class User(AbstractUser):
+    """
+    Our Users inherito from Django's AbstractUser class;
+    it includes the following columns by default:
+
+    username: A field for the username of the user. This field is often used for authentication purposes.
+
+    password: Django stores a hashed version of the user's password in this field. It should never store plain-text passwords. The hashing and password validation are handled by Django's authentication system.
+
+    email: This field stores the email address of the user. It's often used for communication and can be used for password recovery.
+
+    first_name: A field for the first name of the user.
+
+    last_name: A field for the last name of the user.
+
+    is_active: A boolean field used to indicate whether the user is active or not. Inactive users typically cannot log in.
+
+    is_staff: A boolean field used to indicate whether the user has access to the admin interface. Staff users often have administrative privileges.
+
+    is_superuser: A boolean field used to indicate whether the user has superuser privileges. Superusers have full access to all parts of the application.
+
+    date_joined: This field stores the date and time when the user account was created.
+
+    groups: This field is a many-to-many relationship with the Group model, allowing you to assign users to specific groups for permissions management.
+
+    user_permissions: Similar to groups, this field is a many-to-many relationship with the Permission model, enabling fine-grained permissions control for users.
+    """
+    # These are the only columns we need to add:
     bio = models.CharField("bio", max_length=500)
-    display_name = models.CharField(max_length=100, unique=False)
     favorite_show = models.ForeignKey(
         Anime,
         null=True,
@@ -49,6 +76,7 @@ class User(models.Model):
     # blank = True means a form can be submitted without this data
     # idk why it's divied up like that
 
+
 class Comment(models.Model):
     show = models.ForeignKey(
         Anime,
@@ -61,11 +89,14 @@ class Comment(models.Model):
     content = models.CharField(max_length=250)
 
 # this is a custom validator function
+
+
 def validate_rating(val):
-        if 1 <= val <= 5:
-            return val
-        else:
-            raise ValidationError("Ratings must be an integer between 1 and 5")
+    if 1 <= val <= 5:
+        return val
+    else:
+        raise ValidationError("Ratings must be an integer between 1 and 5")
+
 
 class Rating(models.Model):
     show = models.ForeignKey(
@@ -77,6 +108,7 @@ class Rating(models.Model):
         on_delete=models.CASCADE
     )
     star_rating = models.IntegerField(validators=[validate_rating])
+
 
 class Playlist(models.Model):
     # Should playlist names be unique? Currently yes
