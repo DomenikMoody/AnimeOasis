d9e19a4f6d285f155fea885485755046eec4a6a4
Domenik <Domeniktmoody@gmail.com>
2023-09-26 16:56:42 -0400

    seperated

---
+++ AnimeOasis/Frontend/ReactNative/Components/VideoPlayer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+const VidPlayer = () => {
+    return (
+        <View>
+            <Text>
+                This Works
+            </Text>
+        </View>
+    )
+}
+
+export default VidPlayer
