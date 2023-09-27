657b40e8ee46a96b948dbd4d7e6249f5790f3ec2
Domenik <Domeniktmoody@gmail.com>
2023-09-26 20:27:54 -0400

    add Native Controls

--- AnimeOasis/Frontend/ReactNative/App.js
+++ AnimeOasis/Frontend/ReactNative/App.js
@@ -10,6 +10,7 @@ export default function App() {
         source={{
           uri: 'https://www002.vipanicdn.net/streamhls/0789fd4f049c3ca2a49b860ea5d1f456/ep.1.1677591537.480.m3u8',
         }}
+        useNativeControls
       />
     </SafeAreaView>
   );
