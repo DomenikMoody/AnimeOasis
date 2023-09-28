7b6b828aea3155d737c23a6ec81dfd4e53143dcf
Domenik <Domeniktmoody@gmail.com>
2023-09-28 16:43:43 -0400

    working

--- AnimeOasis/Frontend/ReactNative/App.js
+++ AnimeOasis/Frontend/ReactNative/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { Video, ResizeMode } from 'expo-av';
+import VidPlayer from './Components/VideoPlayer';
 
 export default function App() {
   return (
     <SafeAreaView >
       <Text>Words Here !!!!!!!</Text>
+      <VidPlayer />
     </SafeAreaView>
   );
 }
