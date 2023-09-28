f17f376a2063f6b5f0d8cc8673182ab081463373
Domenik <Domeniktmoody@gmail.com>
2023-09-28 16:21:56 -0400

    push with seperate video component

--- AnimeOasis/Frontend/ReactNative/App.js
+++ AnimeOasis/Frontend/ReactNative/App.js
@@ -4,14 +4,8 @@ import { Video, ResizeMode } from 'expo-av';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Video
-        style={styles.video}
-        source={{
-          uri: 'https://www002.vipanicdn.net/streamhls/0789fd4f049c3ca2a49b860ea5d1f456/ep.1.1677591537.480.m3u8',
-        }}
-        useNativeControls
-      />
+    <SafeAreaView >
+      <Text>Words Here !!!!!!!</Text>
     </SafeAreaView>
   );
 }
--- AnimeOasis/Frontend/ReactNative/Components/VideoPlayer.js
+++ AnimeOasis/Frontend/ReactNative/Components/VideoPlayer.js
@@ -1,18 +1,29 @@
-import { View, Text, SafeAreaView} from "react-native";
-import Video from 'react-native-video'
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Video, ResizeMode } from "expo-av";
 
-const VidPlayer = () => {
-    return (
-        <SafeAreaView>
-            <Text>
-                THIS IS WHERE THE VIDEO PLAYER WOULD BE
-            </Text>
-            <Video
-            source= {{uri: "https://www002.vipanicdn.net/streamhls/0789fd4f049c3ca2a49b860ea5d1f456/ep.1.1677591537.480.m3u8"}}
-            >
-            </Video>
-        </SafeAreaView>
-    )
-}
+export default function VidPlayer() {
+  return (
+    <SafeAreaView>
+      <Video
+        style={styles.video}
+        source={{
+          uri: "https://www002.vipanicdn.net/streamhls/0789fd4f049c3ca2a49b860ea5d1f456/ep.1.1677591537.480.m3u8",
+        }}
+        useNativeControls
+      />
 
-export default VidPlayer
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    video: {
+      height: "100%",
+      width: "100%"
+    }
+  });
