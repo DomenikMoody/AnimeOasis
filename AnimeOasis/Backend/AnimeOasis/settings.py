01d86f5fef7aaedef49c6ce37ef46c240ef344bf
Christopher Simpson <simpsonc6@winthropalumni.com>
2023-10-11 17:10:50 -0400

    fixed the example.env and settings.py

--- AnimeOasis/Backend/AnimeOasis/settings.py
+++ AnimeOasis/Backend/AnimeOasis/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = [os.getenv('ALLOW_HOST')]
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 
 CORS_ALLOW_ALL_ORIGINS = False
